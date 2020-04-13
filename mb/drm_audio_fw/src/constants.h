9f5391966185a8bb0864cf277416de0b0498c543
AdnanSlef <39320372+AdnanSlef@users.noreply.github.com>
2020-04-13 21:51:42 +0000

    Version 0.1

--- mb/drm_audio_fw/src/constants.h
+++ mb/drm_audio_fw/src/constants.h
@@ -55,29 +55,34 @@ typedef struct {
 
 // struct to interpret drm metadata
 typedef struct __attribute__((__packed__)) {
-    char md_size;
     char owner_id;
     char num_regions;
     char num_users;
-    char buf[];
+    char rids[MAX_REGIONS];
+    char uids[MAX_USERS];
+    char extra;
+    char iv[16];
 } drm_md;
 
 
-// struct to interpret shared buffer as a drm song file
+// struct to interpret shared buffer as a WAV song file
 // packing values skip over non-relevant WAV metadata
 typedef struct __attribute__((__packed__)) {
     char packing1[4];
     u32 file_size;
     char packing2[32];
     u32 wav_size;
+    char buf[];
+} wav;
+
+
+// struct to interpret shared buffer as a drm song file
+typedef struct __attribute__((__packed__)) {
+    char mac_md[32];
     drm_md md;
-} song;
-
-// accessors for variable-length metadata fields
-#define get_drm_rids(d) (d.md.buf)
-#define get_drm_uids(d) (d.md.buf + d.md.num_regions)
-#define get_drm_song(d) ((char *)(&d.md) + d.md.md_size)
-
+    char mac_c[32];
+    char ct[];
+} drm;
 
 // shared buffer values
 enum commands { QUERY_PLAYER, QUERY_SONG, LOGIN, LOGOUT, SHARE, PLAY, STOP, DIGITAL_OUT, PAUSE, RESTART, FF, RW };
@@ -93,22 +98,24 @@ typedef volatile struct __attribute__((__packed__)) {
     char username[USERNAME_SZ]; // stores logged in or attempted username
     char pin[MAX_PIN_SZ];       // stores logged in or attempted pin
 
-    // shared buffer is either a drm song or a query
+    // shared buffer is either a drm song or a wav song or a query
     union {
-        song song;
+        drm drm;
         query query;
+	wav wav;
     };
 } cmd_channel;
 
 
 // local store for drm metadata
 typedef struct {
-    u8 md_size;
     u8 owner_id;
     u8 num_regions;
-    u8 rids[MAX_REGIONS];
     u8 num_users;
+    u8 rids[MAX_REGIONS];
     u8 uids[MAX_USERS];
+    u8 extra; //TODO is this needed?
+    u8 iv[16]; //TODO is this needed?
 } song_md;
 
 
@@ -117,7 +124,6 @@ typedef struct {
     char logged_in;             // whether or not a user is logged on
     u8 uid;                     // logged on user id
     char username[USERNAME_SZ]; // logged on username
-    char pin[MAX_PIN_SZ];       // logged on pin
     song_md song_md;            // current song metadata
 } internal_state;
 
